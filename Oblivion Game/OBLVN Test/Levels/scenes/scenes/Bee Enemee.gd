51840613b9de3d6282f44e734dac3bf7c839c74a
IFPA <ifpa@CPL-IFPMBP35.local>
2025-12-04 16:23:13 -0500

    new changes to level transition

--- Oblivion Game/OBLVN Test/Levels/scenes/scenes/Bee Enemee.gd
+++ Oblivion Game/OBLVN Test/Levels/scenes/scenes/Bee Enemee.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+
 onready var anim = $Anim
 
 func _ready():
@@ -11,10 +12,3 @@ func _on_Area2D_body_entered(body):
 	if (body.name == "AlexStates"):
 #			anim.play("hit")
 			queue_free()
-#			var y_delta = position.y - body.position.y
-##		if (y_delta > 30):
-##			print("Destroy Enemy")
-#			body.jump()
-#		else:
-#			print("Decrease health")
-#			body.queue_free()
