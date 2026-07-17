69b070eaf38aa67d62fea3de80c584472f08c37b
EdenExists <ehossaena@gmail.com>
2026-07-17 10:12:00 -0400

    Eden's Edits

---
+++ Oblivion Game/OBLVN Updated States/Eden's things/Movement Stuff/Cam.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+export var zoom_speed: float = 4.0
+var target_zoom: Vector2 = Vector2(1, 1)
+var event_zoom: Vector2 = Vector2(0.5, 0.5)
+
+func _ready():
+	current = true
+
+func _physics_process(_delta):
+	set_zoom(target_zoom)
---
+++ Oblivion Game/OBLVN Updated States/Eden's things/NPCs.gd
@@ -0,0 +1,119 @@
+extends Area2D
+
+var tween_dur = 0.7
+var transmode = Tween.TRANS_QUINT
+var easemode = Tween.EASE_IN_OUT
+
+var active = false
+var happened = false
+var entered = false
+var exited = false
+onready var alex = get_node("/root/Main2/AlexStates")
+onready var cam = get_node("/root/Main2/AlexStates/Camera2D")
+onready var t1 = get_node("../AlexStates/Camera2D/T1") #Zoom in
+onready var t2 = get_node("../AlexStates/Camera2D/T2")
+
+func _ready():
+	connect("body_entered", self, "on_NPC_body_entered")
+	connect("body_exited", self, "on_NPC_body_exited")
+	$Icon.visible = false
+
+func _input(event):
+	if (get_node_or_null("DialogNode") == null
+		and event.is_action_pressed("chat")
+		and active
+		and alex.state == alex.States.FLOOR):
+			
+			var place = self.get_global_position() #Where is the cutscene node
+			t1.interpolate_property(cam, "zoom",
+				cam.target_zoom, cam.event_zoom, tween_dur,
+				transmode, easemode, 0)
+			t2.interpolate_property(cam, "global_position",
+				alex.get_global_position(), place, tween_dur,
+				transmode, easemode, 0)
+			t1.start()
+			t2.start()
+			yield(get_tree().create_timer(tween_dur), "timeout") #DO NOT TOUCH THIS UNLESS YOU WANT THE ZOOM TO BREAK
+			get_tree().paused = true
+			if happened:
+				var dialogue = Dialogic.start("Game Dialogue 2")
+				dialogue.pause_mode = Node.PAUSE_MODE_PROCESS
+				dialogue.connect("timeline_end", self, "unpause")
+				add_child(dialogue)
+			else:
+				var dialogue = Dialogic.start("short dialogue 1")
+				dialogue.pause_mode = Node.PAUSE_MODE_PROCESS
+				dialogue.connect("timeline_end", self, "unpause")
+				add_child(dialogue)
+				
+				
+			var anim = get_node("/root/Main2/AlexStates/Anim")
+			anim.play("idle")
+			face_Alex()
+	pass
+
+func face_Alex():
+	if alex.player.x > $Avery.global_position.x:
+		$Avery.set_flip_h(true)
+	if alex.player.x < $Theo.global_position.x:
+		$Theo.set_flip_h(true)
+
+func unpause(EndDialogue):
+	if EndDialogue:
+		
+#		alex.move_and_collide(Vector2(0,0))
+#		cam.set_pause_mode(false)
+		happened = true
+		$Avery.set_flip_h(false)
+		$Theo.set_flip_h(false)
+		t1.interpolate_property(cam, "zoom",
+			cam.event_zoom, cam.target_zoom, tween_dur,
+			transmode, easemode, 0)
+		t2.interpolate_property(cam, "global_position",
+			self.get_global_position(), alex.get_global_position(), tween_dur,
+			transmode, easemode, 0)
+		t1.start()
+		t2.start()
+		cam.set_pause_mode(2)
+		yield(get_tree().create_timer(tween_dur), "timeout")
+		get_tree().paused = false
+		cam.set_pause_mode(0)
+#		cam.set_global_position(get_node("/root/Main2/AlexStates").get_global_position())
+
+func on_NPC_body_entered(body):
+	if body.name == "AlexStates":
+		active = true
+		entered = true
+		exited = false
+
+func on_NPC_body_exited(body):
+	if body.name == "AlexStates":
+		active = false
+		entered = false
+		exited = true
+
+func _process(_delta):
+	if entered:
+		$Icon.visible = true
+		if happened:
+			$Icon.play("2 in")
+			if $Icon.get_frame() == 3:
+				$Icon.stop()
+		else:
+			$Icon.play("1 in")
+			if $Icon.get_frame() == 3:
+				$Icon.stop()
+	if exited:
+		if happened:
+			$Icon.play("2 out")
+			if $Icon.get_frame() == 3:
+				$Icon.stop()
+				$Icon.visible = false
+		else:
+			$Icon.play("1 out")
+			if $Icon.get_frame() == 3:
+				$Icon.stop()
+				$Icon.visible = false
+	
+#	if not active:
+#		$Icon.stop()
